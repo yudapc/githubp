60596e7f524160d93ebe446460a5f775a0e3fabc
Yuda Cogati <yudapc@gmail.com>
2017-04-24 00:17:19 +0700

    refactoring home page

--- src/components/home-page/component.js
+++ src/components/home-page/component.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   Text,
   View,
   TouchableHighlight,
 } from 'react-native';
+
+import { styles } from './styles';
 
 export class HomePage extends Component {
   onPress() {
@@ -30,41 +31,5 @@ export class HomePage extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#6E5BAA',
-  },
-  contentContainer: {
-    marginVertical: 30,
-    paddingTop: 20,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: '#328FE6',
-    padding: 10,
-    marginTop: 10,
-    backgroundColor: '#32c5e6',
-    height: 50,
-  },
-  label: {
-    width: 230,
-    flex: 1,
-    alignSelf: 'center',
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: '600',
-    color: '#ffffff',
-  },
-});
 
 export default HomePage;
