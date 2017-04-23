037e4c0b7a4a3ac198b6eac006ec35e032f36d20
Yuda Cogati <yudapc@gmail.com>
2017-04-24 00:17:48 +0700

    refactoring profile page

--- src/components/profile/component.js
+++ src/components/profile/component.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   Text,
   View,
   TouchableHighlight,
   Image,
 } from 'react-native';
+import { styles } from './styles';
 
 export class Profile extends Component {
   constructor() {
@@ -68,52 +68,5 @@ export class Profile extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#6E5BAA',
-  },
-  loginContainer: {
-    marginVertical: 30,
-    paddingTop: 20,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    width: 250,
-    color: '#555555',
-    padding: 10,
-    height: 50,
-    borderColor: '#32C5E6',
-    borderWidth: 1,
-    borderRadius: 4,
-    alignSelf: 'center',
-    backgroundColor: '#ffffff',
-  },
-  button: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: '#328FE6',
-    padding: 10,
-    marginTop: 10,
-    backgroundColor: '#32c5e6',
-    height: 50,
-  },
-  label: {
-    width: 230,
-    flex: 1,
-    alignSelf: 'center',
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: '600',
-    color: '#ffffff',
-  },
-});
 
 export default Profile;
